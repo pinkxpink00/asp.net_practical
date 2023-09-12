108dcc4ed57cb72cbfb54191244907d323d350f2
pinkxpink00 <kyperu07@gmail.com>
2023-09-12 21:08:01 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp.net_practice.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asp.net_practice.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
