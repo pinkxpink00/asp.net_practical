92f8b689bc55f8d2470adaf292ff18cb444d1ab4
pinkxpink00 <kyperu07@gmail.com>
2023-09-13 19:15:59 +0300

    use IActionResult

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,20 +5,9 @@ namespace asp.net_practice.Controllers
 
 	public class HomeController : Controller
 	{
-		[HttpGet]
-		public async Task Index()
+		public IActionResult Index()
 		{
-			string content = @"<form method = 'post'>
-              <label>Name:</label><br />
-              <input name = 'name' /><br />
-              <label>Age:</label><br />
-              <input type = 'number' name = 'age' /><br />
-              <input type = 'submit' value = 'Send' />
-              </form>";
-			Response.ContentType = "text/html;charset=utf-8";
-			await Response.WriteAsync(content);
+			return new HtmlResult("Hello Kitty");
 		}
-		[HttpPost]
-		public string Index(string name, int age) => $"{name}: {age}:";
 	}
 }
---
+++ HtmlResult.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp.net_practice
+{
+	public class HtmlResult : IActionResult
+	{
+		string html;
+		public HtmlResult(string html) => this.html = html;
+
+		public async Task ExecuteResultAsync(ActionContext context)
+		{
+			string fullHtmlCode = @$"<!DOCTYPE html>
+            <html>
+                <head>
+                    <title>METANIT.COM</title>
+                    <meta charset=utf-8 />
+                </head>
+                <body>{html}</body>
+            </html>";
+			await context.HttpContext.Response.WriteAsync(fullHtmlCode);
+		}
+	}
+}
