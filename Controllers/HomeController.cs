784f3144d1ce157625519ca64f00a00652c600a1
pinkxpink00 <kyperu07@gmail.com>
2023-09-13 15:42:28 +0300

    use POST method

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,16 +2,23 @@
 
 namespace asp.net_practice.Controllers
 {
-	[Controller]
+
 	public class HomeController : Controller
 	{
-		[NonAction] 
-		public string Index()
+		[HttpGet]
+		public async Task Index()
 		{
-			
-			return "Hello World";
+			string content = @"<form method = 'post'>
+              <label>Name:</label><br />
+              <input name = 'name' /><br />
+              <label>Age:</label><br />
+              <input type = 'number' name = 'age' /><br />
+              <input type = 'submit' value = 'Send' />
+              </form>";
+			Response.ContentType = "text/html;charset=utf-8";
+			await Response.WriteAsync(content);
 		}
-
-		public string URL(string name)=>$"your name:{name}";
+		[HttpPost]
+		public string Index(string name, int age) => $"{name}: {age}:";
 	}
 }
